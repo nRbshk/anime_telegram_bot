62d6d57f1f79c29ef2a48d6eeba787621ca800f3
nRbshk <n.ryaboshchuk@gmail.com>
2021-04-07 00:06:52 +0300

    add hello world bot

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('hello world')
+import telebot
+
+
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    # if message.rom_user.id == 900530539:
+    if message.text == "Hello":
+        bot.send_message(message.from_user.id, "POKA")
+
+
+
+bot.polling(none_stop=True, interval=0)
+
