adf90f759a4e32e6517cb176b384bc53751f5e07
nRbshk <n.ryaboshchuk@gmail.com>
2021-04-13 18:12:04 +0300

    SWITCH LOGGER FORMAT

--- BD.py
+++ BD.py
@@ -11,21 +11,23 @@ logger = logging.getLogger(__name__)
 available_status = ["done", "inProgress", "inList", "All"]
 class BD:
     def save_anime(self, idt_, name_, status_, episode_ = 0):
-        logger.info("INSERT INTO BD")
+        logger.info("START save_anime")
         conn = sqlite3.connect("db.db")
         cursor = conn.cursor()
 
         try: 
-            cursor.execute("INSERT INTO anime VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (None, idt_, name_, status_, episode_, "00:00", get_date(), get_date(), "False"))
+            cursor.execute("INSERT INTO anime VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (None, idt_, name_, status_, episode_, "00:00", get_date(), get_date(), episode_, "nb"))
         except:
             return 1
         conn.commit()
         
         conn.close()
+
+        logger.info("START save_anime")
         return 0
 
     def show(self, idt_, status_ = "inProgress"):
-        logger.info("SHOW FROM BD")
+        logger.info("START show")
         conn = sqlite3.connect("db.db")
         cursor = conn.cursor()
 
@@ -40,10 +42,11 @@ class BD:
 
         conn.close()
 
+        logger.info("END show")
         return list_to_print
 
     def set_time(self, idt_, name_, time_):
-        logger.info("SET TIME")
+        logger.info("START set_time")
         conn = sqlite3.connect("db.db")
         cursor = conn.cursor()
 
@@ -55,9 +58,11 @@ class BD:
         conn.commit()
 
         conn.close()
+        logger.info("END set_time")
+
 
     def set_status(self, idt_, name_, status_):
-        logger.info("SET STATUS")
+        logger.info("START set_status")
         conn = sqlite3.connect("db.db")
         cursor = conn.cursor()
 
@@ -71,8 +76,11 @@ class BD:
 
         conn.close()
 
+        logger.info("END set_status")
+
+
     def set_episode(self, idt_, name_, episode_):
-        logger.info("SET EPISODE")
+        logger.info("START set_episode")
         conn = sqlite3.connect("db.db")
         cursor = conn.cursor()
 
@@ -87,14 +95,15 @@ class BD:
         conn.commit()
 
         conn.close()
+        logger.info("END set_episode")
 
 
-    def select_notified_ep(self):
-        logger.info("SELECT notified ep")
+    def select_notified_ep(self, link_loc_: str = "nb"):
+        logger.info("START select_notified_ep")
         conn = sqlite3.connect("db.db")
         cursor = conn.cursor()
 
-        cursor.execute("SELECT * FROM anime WHERE status=?", ("inProgress",))
+        cursor.execute("SELECT * FROM anime WHERE status=? AND link_loc=?", ("inProgress", link_loc_))
 
         response = cursor.fetchall()
         
@@ -105,26 +114,29 @@ class BD:
         for r in response:
             names.append(r[2])
             idt.append(r[1])
-            notified_ep.append(r[-1])
+            notified_ep.append(r[-2])
 
         conn.close()
         del response
         
+        logger.info("END select_notified_ep")
         return names, idt, notified_ep
         
-    def update_notified_ep(self, idt_, name_, notified_ep_):
-        logger.info("UPDATE notified ep")
+    def update_notified_ep(self, idt_, name_, notified_ep_, link_loc_: str ="nb"):
+        logger.info("START update_notified_ep")
         conn = sqlite3.connect("db.db")
         cursor = conn.cursor()
 
         upd_time_ = get_date()
 
-        cursor.execute("UPDATE anime SET notified_ep=? WHERE id_telegram=? AND name=?", (notified_ep_, idt_, name_))
-        cursor.execute("UPDATE anime SET upd_time=? WHERE id_telegram=? AND name=?", (upd_time_, idt_, name_))
+        cursor.execute("UPDATE anime SET notified_ep=? WHERE id_telegram=? AND name=? AND link_loc=?", (notified_ep_, idt_, name_, link_loc_))
+        cursor.execute("UPDATE anime SET upd_time=? WHERE id_telegram=? AND name=? AND link_loc=?", (upd_time_, idt_, name_, link_loc_))
 
         
         conn.commit()
 
         conn.close()
+
+        logger.info("END update_notified_ep")
 
 bd = BD()
