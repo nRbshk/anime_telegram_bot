74cce98024d62aa52309d02fa3e38850b064ead2
nRbshk <n.ryaboshchuk@gmail.com>
2021-04-19 18:50:51 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='telegram bot with python flask server',
+   version='1.0',
+   description='Useful module for chrome extension',
+   author='nRbshk',
+   author_email='n.ryaboshchuK@gmail.com',
+   packages=['telegram bot with python flask server'],
+   install_requires=['flask', 'flask-cors', 'aiogram', 'requests', 'bs4'],
+)
